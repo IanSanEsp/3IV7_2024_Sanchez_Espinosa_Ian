80adeb83c352a8fe57d1e01db1d8811e56de02d7
Ian <isancheze2300@alumno.ipn.mx>
2024-10-26 22:54:07 +0000

    TrasposicionMatriz

--- Algoritmia/2doParcial/Python/04Arreglos/TrasposicionFC.py
+++ Algoritmia/2doParcial/Python/04Arreglos/TrasposicionFC.py
@@ -1,37 +1,79 @@
+#Menu
 def eleccion():
     print("Selecciona una opcion:")
     print("A.- Matriz 3x3")
     print("B.- Matriz 5x5")
+    print("C.- Salir")
     opc = input("....: ").upper()
 
-    return opc
+    if opc == "A" or opc =="B" or opc == "C":
+        return opc
+    else:
+        print("Opcion no valida")
+        return eleccion()
 
-def matriz5x5():
-    matriz5 = []
-
+#Crear matriz 3x3
+def matriz3x3():
+    matriz3 = []
     for f in range(3):
         fila = []
         for c in range(3):
             print("Ingresa el valor de la fila: ", f, ", columna: ", c)
             num = float(input("....: "))
             fila.append(num)
-        matriz5.append(fila)
+        matriz3.append(fila)
 
-    matriz5Tr = []
+    matriz3Tr = []
     for cTr in range(3):
         nFila = []
         for fTr in range(3):
+            numTr= matriz3[fTr][cTr]
+            nFila.append(numTr)
+        matriz3Tr.append(nFila)
+
+    return matriz3, matriz3Tr
+
+#Matriz 5x5
+def matriz5x5():
+    matriz5 = []
+    for f in range(5):
+        fila = []
+        for c in range(5):
+            print("Ingresa el valor de la fila: ", f, ", columna: ", c)
+            num = float(input("....: "))
+            fila.append(num)
+        matriz5.append(fila)
+
+    matriz5Tr = []
+    for cTr in range(5):
+        nFila = []
+        for fTr in range(5):
             numTr= matriz5[fTr][cTr]
             nFila.append(numTr)
         matriz5Tr.append(nFila)
 
-    for f in matriz5:
+    return matriz5, matriz5Tr
+
+#Imprimir matriz
+def impMatriz(matriz, matrizTr):
+    print("La matriz original es: ")
+    for f in matriz:
         print(f)
 
     print("")
-    for f in matriz5Tr:
+
+    print("La trasposicion de filas y columnas es: ")
+    for f in matrizTr:
         print(f)
 
-    print(matriz5[0][0])
 #Principal
-matriz5x5()
+opc = eleccion()
+
+if opc == "A":
+    matriz, matrizTr = matriz3x3()
+    impMatriz(matriz, matrizTr)
+elif opc == "B":
+    matriz, matrizTr = matriz5x5()
+    impMatriz(matriz, matrizTr)
+elif opc == "C":
+    print("Programa cerrado")
